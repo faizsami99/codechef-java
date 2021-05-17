b3f0d4ab6a6332266de0c461aaecce0300a6b6d7
faizsami99 <68805461+faizsami99@users.noreply.github.com>
2021-05-17 21:24:35 +0530

    Add files via upload

---
+++ MUFFINS3.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class MUFFINS3{
+	public static void main(String[] args) {
+	  	Scanner scr = new Scanner(System.in);
+        int t = scr.nextInt();
+        for (int i=0;i<t;i++){
+            int n = scr.nextInt();
+            int d = (n/2)+1;
+            System.out.println(d);
+     	}
+	}
+}
+
